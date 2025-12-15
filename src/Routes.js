7e09b37e765bc3bad7867ece629fece9e5635003
ronaldo <ronaldoreemias@gmail.com>
2025-12-15 01:40:44 -0300

    terceira tentativa

--- src/Routes.js
+++ src/Routes.js
@@ -1,6 +1,5 @@
 import { BrowserRouter, Routes, Route} from "react-router-dom";
 import Home from "./pages/Home";
-import Loja from "./pages/loja";
 import Vagas from "./pages/Vagas";
 import Forum from "./pages/Forum";
 import Notificacao from "./pages/Notificacao";
@@ -11,7 +10,6 @@ function AppRoutes(){
             <Routes>
                 <Route path="/" element={ <Home/> } ></Route>
                 <Route path="/Notficacao" element={ <Notificacao/> } ></Route>
-                <Route path="/Loja" element={ <Loja/> } ></Route>
                 <Route path="/Forum" element={ <Forum/> } ></Route>
                 <Route path="/Vagas" element={ <Vagas/> } ></Route>
             </Routes>
