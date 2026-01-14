50132f83bde8d424fb428fb801fdcb8dcac1947e
ronaldo <ronaldoreemias@gmail.com>
2026-01-14 02:57:29 -0300

    upload: lixuPostagens

--- src/Components/Lixudospostes/index.tsx
+++ src/Components/Lixudospostes/index.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 function DeleteButton() {
   const handleDelete = async () => {
     try {
--- src/pages/dhanteconfig/index.tsx
+++ src/pages/dhanteconfig/index.tsx
@@ -1,167 +1,73 @@
 import style from "./Dante.module.css";
-import React, { useState } from "react";
-import Apagar from "../../Components/Lixudospostes"
+import { useState } from "react";
+import Apagar from "../../Components/Lixudospostes";
 
-class PostagemcomFotonoBlog extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      texto: '',
-      link: '',
-      autor: '',
-      file: null
-    };
+export default function Cms() {
+  const [texto, setTexto] = useState("");
+  const [link, setLink] = useState("");
+  const [autor, setAutor] = useState("");
+  const [file, setFile] = useState<File | null>(null);
 
-    this.handleFile = this.handleFile.bind(this);
-    this.handleAutor = this.handleAutor.bind(this);
-    this.handleLink = this.handleLink.bind(this);
-    this.handleTexto = this.handleTexto.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
+  const handleTexto = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setTexto(e.target.value);
+  };
 
-  handleFile(event) {
-    this.setState({ file: event.target.files[0] });
-  }
+  const handleLink = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setLink(e.target.value);
+  };
 
-  handleTexto(event) {
-    this.setState({ texto: event.target.value });
-  }
+  const handleAutor = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setAutor(e.target.value);
+  };
 
-  handleLink(event) {
-    this.setState({ link: event.target.value });
-  }
+  const handleFile = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (!e.target.files || e.target.files.length === 0) return;
+    setFile(e.target.files[0]);
+  };
 
-  handleAutor(event) {
-    this.setState({ autor: event.target.value });
-  }
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
 
-  handleSubmit(event) {
-    event.preventDefault();
     const formData = new FormData();
-    formData.append('file', this.state.file);
-    formData.append('texto', this.state.texto);
-    formData.append('link', this.state.link);
-    formData.append('autor', this.state.autor);
+    formData.append("texto", texto);
+    formData.append("link", link);
+    formData.append("autor", autor);
 
-    alert('Atualizando: ' + this.state.texto);
-  }
+    if (file) {
+      formData.append("file", file);
+    }
 
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit} className={style.formulariocomfoto}>
-            <div className={style.P_do_forme}>
-                <p>Formulario de postagem com foto</p>
-            </div>
-        
-            <div>
-                <p>o que deseja escrever :</p>
-                <textarea value={this.state.texto} onChange={this.handleTexto} />
-            </div>
-        
-            <div>
-                <p>link de onde buscou a informação</p>
-                <input type="text" value={this.state.link} onChange={this.handleLink} />
-            </div>
-        
-            <div>
-                <p>Nome do autor</p>
-                <input type="text" value={this.state.autor} onChange={this.handleAutor} />
-            </div>
-            <div>
-                <p>escolha uma foto para sua postagem</p>
-                <input type="file" name="file" onChange={this.handleFile} />
-            </div>        
-        <input type="submit" value="enviar" />
-        <Apagar />
-      </form>
-    );
-  }
+    alert("Atualizando: " + texto);
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className={style.formulariocomfoto}>
+      <div className={style.P_do_forme}>
+        <p>Formulario de postagem com foto</p>
+      </div>
+
+      <div>
+        <p>o que deseja escrever :</p>
+        <textarea value={texto} onChange={handleTexto} />
+      </div>
+
+      <div>
+        <p>link de onde buscou a informação</p>
+        <input type="text" value={link} onChange={handleLink} />
+      </div>
+
+      <div>
+        <p>Nome do autor</p>
+        <input type="text" value={autor} onChange={handleAutor} />
+      </div>
+
+      <div>
+        <p>escolha uma foto para sua postagem</p>
+        <input type="file" onChange={handleFile} />
+      </div>
+
+      <input type="submit" value="enviar" />
+      <Apagar />
+    </form>
+  );
 }
-
-
-class PostagemnoBlog extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      texto: '',
-      link: '',
-      autor: ''
-    };
-
-    this.handleAutor = this.handleAutor.bind(this);
-    this.handleLink = this.handleLink.bind(this);
-    this.handleTexto = this.handleTexto.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-
-  handleTexto(event) {
-    this.setState({ texto: event.target.value });
-  }
-
-  handleLink(event) {
-    this.setState({ link: event.target.value });
-  }
-
-  handleAutor(event) {
-    this.setState({ autor: event.target.value });
-  }
-
-  handleSubmit(event) {
-    event.preventDefault();
-    const formData = new FormData();
-    formData.append('texto', this.state.texto);
-    formData.append('link', this.state.link);
-    formData.append('autor', this.state.autor);
-
-    alert('Atualizando: ' + this.state.texto);
-  }
-
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit} className={style.formulariosemfoto}>
-        <div className={style.P_do_forme}>
-          <p>Formulario de postagem sem foto</p>
-        </div>
-        
-            <div>
-                <p>o que deseja escrever :</p>
-                <textarea value={this.state.texto} onChange={this.handleTexto} />
-            </div>
-        
-          <div>
-            <p>link de onde buscou a informação</p>
-            <input type="text" value={this.state.link} onChange={this.handleLink} />
-          </div>
-        
-        <div>
-            <p>Nome do autor</p>
-            <input type="text" value={this.state.autor} onChange={this.handleAutor} />
-        </div>
-        <input type="submit" value="enviar" />
-      </form>
-    );
-  }
-}
-
-
-function Cms(){
-    return (
-        
-        <div className={style.painelConstrole}>
-            <br/>
-            <br/>
-            <br/>
-            <div className={style.contanderform}>
-                <PostagemcomFotonoBlog />
-                <PostagemnoBlog />
-            </div>
-            <br/>
-            
-            <br/>
-
-        </div>
-    );
-}
-
-export default Cms;
